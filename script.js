7f3558b165d2c5bf632d9b8815c61d58f08a7e83
Msheikh <m.sheikhnoor17@gmail.com>
2023-01-06 22:04:13 +0000

    simplified script with functions

--- script.js
+++ script.js
@@ -2,63 +2,57 @@
 const books = document.querySelector(".books");
 const addBook = document.querySelector("#button");
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-    };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R Toliken", 295, "not read yet");
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages`;
+};
 
-const theLOTR = new Book(
-    "The Lord of The Rings",
-    "J.R.R Toliken",
-    300,
-    "not read yet"
-);
-const harryPotter = new Book(
-    "Harry Potter",
-    "J.K Rowling",
-    1000,
-    "not read yet"
-);
-const noCountry = new Book(
-    "No Country for Old Men",
-    "Cormac McCarthy",
-    1000,
-    "read"
-);
-const goneGirl = new Book("Gone Girl", "Gilian Flyn", 300, "Not read yet");
+const theHobbit = new Book("The Hobbit", "J.R.R Toliken", 295);
+const theLOTR = new Book("The Lord of The Rings", "J.R.R Toliken", 300);
+const harryPotter = new Book("Harry Potter", "J.K Rowling", 1000);
+const noCountry = new Book("No Country for Old Men", "Cormac McCarthy", 1000);
+const goneGirl = new Book("Gone Girl", "Gilian Flyn", 300);
 
 let myLibrary = [theHobbit, harryPotter, theLOTR, noCountry, goneGirl];
 
-addBook.addEventListener("click", (e) => {
-    e.preventDefault();
-    const nameOfBook = document.querySelector("#nameOfBook").value;
-    const author = document.querySelector("#author").value;
-    const numberOfPages = document.querySelector("#noOfPages").value;
-    const readOrNot = document.querySelector("#read").value;
-    const addBook = new Book(nameOfBook, author, numberOfPages, readOrNot);
-    myLibrary.push(addBook);
-    console.log(myLibrary);
-    // addBookToLibrary(myLibrary);
-    const newBooks = document.createElement("div");
-    newBooks.classList.add("book");
-    newBooks.innerHTML = addBook.info();
-    books.appendChild(newBooks);
+let myNewBooks = [];
+
+function newBooksToAdd(lib) {
+    lib.forEach((book) => {
+        myNewBooks.push(book);
+        newBookSection(book);
+    });
+}
+
+newBooksToAdd(myLibrary);
+
+function newBookSection(element) {
+    const newBookdiv = document.createElement("div");
+    newBookdiv.classList.add("book");
+    newBookdiv.innerHTML = element.info();
+    books.appendChild(newBookdiv);
+    removeButton(newBookdiv);
+    readOrNot(newBookdiv);
+}
+
+function removeButton(div) {
     const removeButton = document.createElement("button");
     removeButton.classList.add("removeButton");
     removeButton.textContent = "Remove Book";
-    newBooks.appendChild(removeButton);
+    div.appendChild(removeButton);
     removeButton.addEventListener("click", () => {
-        books.removeChild(newBooks);
+        books.removeChild(div);
     });
+}
+
+function readOrNot(div) {
     const readOrNotbutton = document.createElement("button");
-    newBooks.appendChild(readOrNotbutton);
+    div.appendChild(readOrNotbutton);
     readOrNotbutton.textContent = "Not Read";
     readOrNotbutton.classList.add("readOrNot");
     readOrNotbutton.addEventListener("click", () => {
@@ -70,41 +64,17 @@ addBook.addEventListener("click", (e) => {
             readOrNotbutton.style.backgroundColor = "chocolate";
         }
     });
-});
-
-function addBookToLibrary(lib) {
-    lib.forEach((element) => {
-        const newBook = document.createElement("div");
-        newBook.classList.add("book");
-        newBook.innerHTML = element.info();
-        books.appendChild(newBook);
-        const removeButton = document.createElement("button");
-        removeButton.classList.add("removeButton");
-        removeButton.textContent = "Remove Book";
-        newBook.appendChild(removeButton);
-        removeButton.addEventListener("click", () => {
-            books.removeChild(newBook);
-        });
-        const readOrNotbutton = document.createElement("button");
-        newBook.appendChild(readOrNotbutton);
-        readOrNotbutton.textContent = "Not Read";
-        readOrNotbutton.classList.add("readOrNot");
-        readOrNotbutton.addEventListener("click", () => {
-            if (readOrNotbutton.textContent === "Not Read") {
-                readOrNotbutton.textContent = "Read";
-                readOrNotbutton.style.backgroundColor = "green";
-            } else {
-                readOrNotbutton.textContent = "Not Read";
-                readOrNotbutton.style.backgroundColor = "chocolate";
-            }
-        });
-    });
 }
 
-addBookToLibrary(myLibrary);
-
-// let myNewBooks = [];
-
-// function newBooksToAdd() {}
-
-// function addTomyNewBooks() {}
+addBook.addEventListener("click", (e) => {
+    e.preventDefault();
+    const nameOfBook = document.querySelector("#nameOfBook").value;
+    const author = document.querySelector("#author").value;
+    const numberOfPages = document.querySelector("#noOfPages").value;
+    const readOrNot = document.querySelector("#read").value;
+    const addBook = new Book(nameOfBook, author, numberOfPages, readOrNot);
+    myNewBooks.push(addBook);
+    newBookSection(addBook);
+    removeButton();
+    readOrNot();
+});
